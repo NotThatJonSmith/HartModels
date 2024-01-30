69505c54fb99174057945883ca0092237631cbd8
Jonathan Smith <jonathan@Jonathans-MBP.attlocal.net>
2024-01-29 20:51:35 -0600

    Virt to Host cache transactor

--- include/OptimizedHart.hpp
+++ include/OptimizedHart.hpp
@@ -4,55 +4,35 @@
 #include <cstdint>
 
 #include <Hart.hpp>
-
-#include <Translators/DirectTranslator.hpp>
-#include <Transactors/DirectTransactor.hpp>
-#include <Transactors/TranslatingTransactor.hpp>
 #include <Decoders/PrecomputedDecoder.hpp>
-#include <Translators/CacheWrappedTranslator.hpp>
+#include <Transactors/VirtToHostTransactor.hpp>
 
 // TODO accelerated WFI states?
-template<
-    typename XLEN_t,
-    unsigned int TranslationCacheSizePoT,
-    bool SkipBusForFetches,
-    unsigned int maxBasicBlockLength,
-    unsigned int numNextBlocks,
-    unsigned int bbCacheSizePoT
-    // TODO optional (always on right now):  bool SkipBusForPageTables
->
+template<typename XLEN_t>
 class OptimizedHart final : public Hart<XLEN_t> {
 
 private:
 
-    DirectTransactor<XLEN_t> busPATransactor;
-    DirectTransactor<XLEN_t> memPATransactor;
-    DirectTranslator<XLEN_t> memTranslator;
-    DirectTranslator<XLEN_t> busTranslator;
-    CacheWrappedTranslator<XLEN_t, TranslationCacheSizePoT> cachedTranslator;
-    TranslatingTransactor<XLEN_t, false> busVATransactor;
-    TranslatingTransactor<XLEN_t, false> memVATransactor;
+    static constexpr unsigned int icacheBits = 14;
+    static constexpr unsigned int virtHostCacheBits = 8;
+
     PrecomputedDecoder<XLEN_t> decoder;
+    VirtToHostTransactor<XLEN_t, virtHostCacheBits> transactor;
+    // TODO turn on bufferTransactions later on.
 
     struct SimplyCachedInstruction {
         XLEN_t full_pc;
         __uint32_t encoding = 0;
         DecodedInstruction<XLEN_t> instruction = nullptr;
     };
-    SimplyCachedInstruction icache[0x10000];
+    SimplyCachedInstruction icache[1<<icacheBits];
 
 public:
 
-    OptimizedHart(CASK::IOTarget* bus, CASK::IOTarget* mem, __uint32_t maximalExtensions) :
+    OptimizedHart(CASK::IOTarget* bus, __uint32_t maximalExtensions) :
         Hart<XLEN_t>(maximalExtensions),
-        busPATransactor(bus),
-        memPATransactor(mem),
-        memTranslator(&this->state, &memPATransactor),
-        busTranslator(&this->state, &busPATransactor),
-        cachedTranslator(&memTranslator),
-        busVATransactor(&cachedTranslator, &busPATransactor),
-        memVATransactor(&cachedTranslator, &memPATransactor),
-        decoder(&this->state) {
+        decoder(&this->state),
+        transactor(bus, &this->state) {
         this->state.implCallback = std::bind(&OptimizedHart::Callback, this, std::placeholders::_1);
         // TODO callback for changing XLENs
         Reset();
@@ -60,24 +40,19 @@ public:
 
     virtual inline unsigned int Tick() override {
         for (unsigned int i = 0; i < 10000; i++) {
-            SimplyCachedInstruction inst = icache[(this->state.pc >> 1) & 0xffff];
+            SimplyCachedInstruction inst = icache[(this->state.pc >> 1) & ((1<<icacheBits)-1)];
             if (inst.full_pc == this->state.pc) [[ likely ]] {
-                inst.instruction(inst.encoding, &this->state, &busVATransactor);
+                inst.instruction(inst.encoding, &this->state, &transactor);
+                continue;
+            }
+            __uint32_t encoding;
+            Transaction<XLEN_t> transaction = transactor.Fetch(this->state.pc, sizeof(encoding), (char*)&encoding);
+            if (transaction.trapCause == RISCV::TrapCause::NONE) {
+                DecodedInstruction<XLEN_t> decoded = decoder.Decode(encoding);
+                icache[(this->state.pc >> 1) & ((1<<icacheBits)-1)] = { this->state.pc, encoding, decoded };
+                decoded(encoding, &this->state, &transactor);
             } else {
-                __uint32_t encoding;
-                Transaction<XLEN_t> transaction;
-                if constexpr (SkipBusForFetches) {
-                    transaction = memVATransactor.Fetch(this->state.pc, sizeof(encoding), (char*)&encoding);
-                } else {
-                    transaction = busVATransactor.Fetch(this->state.pc, sizeof(encoding), (char*)&encoding);
-                }
-                if (transaction.trapCause == RISCV::TrapCause::NONE) {
-                    DecodedInstruction<XLEN_t> decoded = decoder.Decode(encoding);
-                    icache[(this->state.pc >> 1) & 0xffff] = { this->state.pc, encoding, decoded };
-                    decoded(encoding, &this->state, &busVATransactor);
-                } else {
-                    this->state.RaiseException(transaction.trapCause, this->state.pc);
-                }
+                this->state.RaiseException(transaction.trapCause, this->state.pc);
             }
         }
         return 10000;
@@ -85,42 +60,20 @@ public:
 
     virtual inline void Reset() override {
         this->state.Reset(this->resetVector);
-        cachedTranslator.Clear();
+        transactor.Clear();
         decoder.Configure(&this->state);
         memset(icache, 0, sizeof(icache));
     };
 
     virtual inline Transactor<XLEN_t>* getVATransactor() override {
-        return &busVATransactor;
+        return &this->transactor;
     }
 
 private:
 
-    static inline bool instructionCanBranch(DecodedInstruction<XLEN_t> instruction) {
-        // TODO, maybe some of these aren't 100% necessarily basic-block-breakers
-        // TODO perf: this check can be abolished if there is a hart callback for "I might branch" or some other slickness
-        return (
-            instruction == ex_jal<XLEN_t>     || instruction == ex_jalr<XLEN_t>         ||
-            instruction == ex_fence<XLEN_t>   || instruction == ex_fencei<XLEN_t>       ||
-            instruction == ex_sfencevma<XLEN_t> ||
-            instruction == ex_branch_generic<XLEN_t, std::equal_to<XLEN_t>>      ||
-            instruction == ex_branch_generic<XLEN_t, std::not_equal_to<XLEN_t>>  ||
-            instruction == ex_branch_generic<XLEN_t, std::less<XLEN_t>>           ||
-            instruction == ex_branch_generic<XLEN_t, std::greater_equal<XLEN_t>>  ||
-            instruction == ex_branch_generic<XLEN_t, std::less<std::make_signed_t<XLEN_t>>>          ||
-            instruction == ex_branch_generic<XLEN_t, std::greater_equal<std::make_signed_t<XLEN_t>>> ||
-            instruction == ex_trap_return<XLEN_t, RISCV::PrivilegeMode::Machine> ||
-            instruction == ex_trap_return<XLEN_t, RISCV::PrivilegeMode::Supervisor> ||
-            instruction == ex_trap_return<XLEN_t, RISCV::PrivilegeMode::User> ||
-            instruction == ex_cjal<XLEN_t>    || instruction == ex_cj<XLEN_t>        ||
-            instruction == ex_cbeqz<XLEN_t>   || instruction == ex_cbnez<XLEN_t>     ||
-            instruction == ex_cjalr<XLEN_t>   || instruction == ex_cjr<XLEN_t>
-        );
-    }
-
     inline void Callback(HartCallbackArgument arg) {
         if (arg == HartCallbackArgument::RequestedVMfence)
-            cachedTranslator.Clear();
+            transactor.Clear();
         if (arg == HartCallbackArgument::RequestedIfence || arg == HartCallbackArgument::RequestedVMfence)
             memset(icache, 0, sizeof(icache));
         if (arg == HartCallbackArgument::ChangedMISA) {
